47e0961de5f3ac171b12585639f9550afa0b9f1b
juan castañeda <juanemporis123>
2022-05-30 17:41:31 -0500

    Initial Commit

---
+++ Networkng/NetworkingProvider.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkingProvider.swift
+//  Networkng
+//
+//  Created by wendy manrique on 30/05/22.
+//
+
+import Foundation
+
+final class NetworkingProvider {
+    
+    static let shared = NetworkingProvider()
+}
---
+++ Networkng/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Networkng
+//
+//  Created by wendy manrique on 30/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+}
+
